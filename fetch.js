f3ea3703bf33845b536937ca08440285267e44a7
Tobias Feld <tobias.feld@hotmail.com>
2018-03-26 21:22:52 +0200

    Updated fetching data script
    Implementing data in the index page

--- fetch.js
+++ fetch.js
@@ -1,14 +1,14 @@
 var request = require("request");
 var fs = require("fs")
-var YAML = require('json2yaml')
-var endpoint = "https://spark-stats-homepage-yszvjzhnmp6p.runkit.sh/"
+var endpoint = "https://runkit.io/tobiasfeld22/spark-stats-homepage/2.0.0"
 
 request({ uri: endpoint, json: true }, (error, response, body) => {
-    if (error || response.statusCode !== 200) { return }
-    output = YAML.stringify(body);
-
+    if (error || response.statusCode !== 200) { process.exit(1) }
+    var keys = Object.keys(body.stats)
+    var values = Object.values(body.stats)
+    var output = keys.map((i, n) => (`${i}: ${values[n]}`)).join("\n")
     fs.writeFile("./_data/stats.yml", output, function(err) {
-        console.error(err)
+        if (err) { return process.exit(1) }
+        console.error("Fetching data succeeded.")
     })
-
 })
